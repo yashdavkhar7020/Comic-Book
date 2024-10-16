f997e9bef50ab788e325012dbbbd88f853a2700c
Yash Shivaji davkhar <yashdavkhar007@gmail.com>
2024-10-16 17:46:55 +0530

    Update comicBookRoutes.js

--- comicBookRoutes.js
+++ comicBookRoutes.js
@@ -3,19 +3,15 @@ const express = require('express');
 const router = express.Router();
 const comicBookController = require('../controllers/comicBookController');
 
-// Create a new Comic Book
+
 router.post('/', comicBookController.createComicBook);
 
-// Get all Comic Books
 router.get('/', comicBookController.getAllComicBooks);
 
-// Get a single Comic Book by ID
 router.get('/:id', comicBookController.getComicBookById);
 
-// Update a Comic Book
 router.put('/:id', comicBookController.updateComicBook);
 
-// Delete a Comic Book
 router.delete('/:id', comicBookController.deleteComicBook);
 
 module.exports = router;
