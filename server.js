1c6babdd55fe2519a2b6ff3cd907df027cce3002
Yash Shivaji davkhar <yashdavkhar007@gmail.com>
2024-10-16 17:47:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,28 +3,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const helmet = require('helmet'); // Optional for security
+const helmet = require('helmet'); 
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
-app.use(helmet()); // Optional for security
+app.use(helmet());
 
-// Routes
+
 const comicBookRoutes = require('./routes/comicBookRoutes');
 app.use('/api/comics', comicBookRoutes);
 
-// Error Handling Middleware
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// Connect to MongoDB and start the server
+
 const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI, { 
     useNewUrlParser: true, 
